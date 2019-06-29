68505ef9e66cc361d6a45625feb72b7e55a9b6a9
rodrigo claro <zembrzuski@gmail.com>
2019-06-29 16:38:08 -0300

    fix no capital social e melhorias no processo de importacao

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from services import downloader_batch
+from services import balanco_importer
 
 
 def main():
-    downloader_batch.download()
+    # downloader_batch.download()
+    # balanco_importer.import_balanco(81551) # dimed
+    # balanco_importer.import_balanco(80929) # petrobras
+    balanco_importer.import_balanco(80981)  # hering
+
     print('finished')
 
 
--- xml_extractors/composicao_capital_social_extractor.py
+++ xml_extractors/composicao_capital_social_extractor.py
@@ -1,6 +1,31 @@
 import xmltodict
 
 
+def __capital_social_converter(capital_social_dict):
+    return {
+        'capital_integralizado': {
+            'on': int(capital_social_dict['QuantidadeAcaoOrdinariaCapitalIntegralizado']),
+            'pn': int(capital_social_dict['QuantidadeAcaoPreferencialCapitalIntegralizado']),
+            'total': int(capital_social_dict['QuantidadeTotalAcaoCapitalIntegralizado'])
+        },
+        'tesouraria': {
+            'on': int(capital_social_dict['QuantidadeAcaoOrdinariaTesouraria']),
+            'pn': int(capital_social_dict['QuantidadeAcaoPreferencialTesouraria']),
+            'total': int(capital_social_dict['QuantidadeTotalAcaoTesouraria'])
+        }
+    }
+
+
+def __capital_social_list_filter(composicao_capital_social_list):
+    filtered = list(filter(
+        lambda x: int(x['QuantidadeAcaoOrdinariaCapitalIntegralizado']) > 0, composicao_capital_social_list))
+
+    if len(filtered) != 1:
+        raise Exception('caso nao previsto ainda')
+
+    return __capital_social_converter(filtered[0])
+
+
 def extract_information(xml):
     the_dict = xmltodict.parse(xml.decode('utf-8'))
 
@@ -12,15 +37,10 @@ def extract_information(xml):
 
     capital_social = array['ComposicaoCapitalSocialDemonstracaoFinanceira']
 
-    return {
-        'capital_integralizado': {
-            'on': int(capital_social['QuantidadeAcaoOrdinariaCapitalIntegralizado']),
-            'pn': int(capital_social['QuantidadeAcaoPreferencialCapitalIntegralizado']),
-            'total': int(capital_social['QuantidadeTotalAcaoCapitalIntegralizado'])
-        },
-        'tesouraria': {
-            'on': int(capital_social['QuantidadeAcaoOrdinariaTesouraria']),
-            'pn': int(capital_social['QuantidadeAcaoPreferencialTesouraria']),
-            'total': int(capital_social['QuantidadeTotalAcaoTesouraria'])
-        }
-    }
+    if isinstance(capital_social, list):
+        return __capital_social_list_filter(capital_social)
+    elif isinstance(capital_social, dict):
+        return __capital_social_converter(capital_social)
+    else:
+        raise Exception('caso nao previsto ainda')
+
