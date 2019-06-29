fafe4bec4b8496791fedfd9802c0439d7bf5cd2c
rodrigo claro <zembrzuski@gmail.com>
2019-06-29 09:08:14 -0300

    loading filesystem file

--- helpers/filesystem_helper.py
+++ helpers/filesystem_helper.py
@@ -1,16 +1,31 @@
 import os
+from pathlib import Path
+
+base_filesystem_path = '/home/zembrzuski/labs/rolling-snow-zips'
+
+
+def find_filepath_by_id_documento(id_documento):
+    zip_file_name = '{}.zip'.format(id_documento)
+
+    for filename in Path(base_filesystem_path).glob('**/{}'.format(zip_file_name)):
+        return filename
+
+    raise Exception("File not found {}".format(id_documento))
 
 
 def load_file(id_documento):
-    print('ae')
+    file_path = find_filepath_by_id_documento(id_documento)
+
+    with open(str(file_path), 'rb') as f:
+        return f.read()
 
 
 def persist_file(codigo_cvm, file_index, file_to_persist):
-    directory = '{}/{}'.format('/home/zembrzuski/labs/rolling-snow-zips', codigo_cvm)
+    directory = '{}/{}'.format(base_filesystem_path, codigo_cvm)
 
     os.makedirs(directory, exist_ok=True)
 
     with open('{}/{}.zip'.format(directory, file_index), 'wb') as f:
         f.write(file_to_persist)
 
-    return None
+    return True
