05dad5ad4ba8788eb875aca0823336d6bbf80e00
rodrigo claro <zembrzuski@gmail.com>
2019-06-29 16:52:06 -0300

    vai

--- xml_extractors/balanco_xml_extractor.py
+++ xml_extractors/balanco_xml_extractor.py
@@ -5,6 +5,7 @@ from xml_extractors import info_financeiras_extractor
 
 def extract_balanco(all_files):
     return {
+        'document_type': all_files['document_type'],
         'formulario_cadastral': formulario_cadastral_extractor.extract(all_files['formulario_cadastral']),
         'informacoes_financeiras': info_financeiras_extractor.extract(all_files['informacoes_financeiras']),
         'composicao_capital_social': composicao_capital_social_extractor.extract(all_files['composicao_capital_social'])
