47a63f752ef1089960b9cb7621e3cea9a7f5709f
rodrigo claro <zembrzuski@gmail.com>
2019-06-29 22:10:21 -0300

    ino

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ from services import balanco_importer
 
 def main():
     # downloader_batch.download()
-    balanco_importer.import_balanco(81551)  # dimed DFP
-    # balanco_importer.import_balanco(60695)  # 60695 ITR petro
-    # balanco_importer.import_balanco(63571)      # DFP petro
-    # balanco_importer.import_balanco(69690)      # ITR dimed
+    # balanco_importer.import_balanco(81551)  # dimed DFP
+
+    # balanco_importer.import_balanco(69690)      # ITR dimed mes 09
+    # balanco_importer.import_balanco(68182)      # ITR dimed mes 06
+    balanco_importer.import_balanco(65643)      # ITR dimed mes 03
+
     print('finished')
 
 
