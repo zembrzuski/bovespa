3a54be7ea7aaf3d06921894cdd6aa8f9ca3f3880
rodrigo claro <zembrzuski@gmail.com>
2019-06-30 20:46:16 -0300

    esta corretissimo agora

--- helpers/conta_extractor_helper.py
+++ helpers/conta_extractor_helper.py
@@ -1,24 +1,34 @@
 import re
 
-regexes = {
-    'patrimonio_liquido': '^patr(\.)?(im[oô]nio)? l[ií]q(\.)?(uido)?$',
-    'ativo_total': '^ativo total$'
+conta_filter = {
+    'patrimonio_liquido': {
+        'regex': '^patr(\.)?(im[oô]nio)? l[ií]q(\.)?(uido)?$',
+        'codigo_tipo_informacao_financeira': 1
+    },
+    'ativo_total': {
+        'regex': '^ativo total$',
+        'codigo_tipo_informacao_financeira': 2
+    }
 }
 
 
 def evaluate_indicador(conta, txt):
-    return re.search(regexes[conta], txt, re.IGNORECASE)
+    return re.search(conta_filter[conta]['regex'], txt, re.IGNORECASE)
 
 
 def retrieve_a_given_conta(conta, informacoes_financeiras):
-    filtered = list(filter(
+    filtered_descricao_conta = list(filter(
         lambda x: evaluate_indicador(conta, x['descricao_conta']),
         informacoes_financeiras))
 
-    if len(filtered) != 1:
+    filtered_info_financeira = list(filter(
+        lambda x: x['codigo_tipo_informacao_financeira'] == conta_filter[conta]['codigo_tipo_informacao_financeira'],
+        filtered_descricao_conta))
+
+    if len(filtered_info_financeira) != 1:
         raise Exception('errow. melhore tua regex!')
 
-    return filtered[0]
+    return filtered_info_financeira[0]
 
 
 def retrieve_conta_with_date(conta, informacoes_financeiras, index_to_date_mapper):
--- services/xml_extractors/info_financeiras_extractor.py
+++ services/xml_extractors/info_financeiras_extractor.py
@@ -2,9 +2,12 @@ import xmltodict
 
 
 def extract_conta(an_account):
+    codigo_info_financeira = int(an_account['PlanoConta']['VersaoPlanoConta']['CodigoTipoInformacaoFinanceira'])
+
     return {
         'plano_conta': an_account['PlanoConta']['NumeroConta'],
         'descricao_conta': an_account['DescricaoConta1'],
+        'codigo_tipo_informacao_financeira': codigo_info_financeira,
         'valores_conta': [
             float(an_account['ValorConta1']),
             float(an_account['ValorConta2']),
@@ -18,8 +21,5 @@ def extract_conta(an_account):
 def extract(info_financeiras):
     the_dict = xmltodict.parse(info_financeiras.decode('utf-8'))
     info_finas_array = the_dict['ArrayOfInfoFinaDFin']['InfoFinaDFin']
-    informacoes_financeiras_consolidadas = list(filter(
-        lambda x: x['PlanoConta']['VersaoPlanoConta']['CodigoTipoInformacaoFinanceira'] == '1', info_finas_array
-    ))
 
-    return list(map(lambda an_account: extract_conta(an_account), informacoes_financeiras_consolidadas))
+    return list(map(lambda an_account: extract_conta(an_account), info_finas_array))
