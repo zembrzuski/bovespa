819b49fb220cae6c250bb8c7669cdad84631e95c
rodrigo claro <zembrzuski@gmail.com>
2019-06-30 20:24:46 -0300

    datas levemente melhoradas

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from services import balanco_importer
 
 def main():
     # downloader_batch.download()
-    # balanco_importer.import_balanco(81551)  # dimed DFP
+    balanco_importer.import_balanco(81551)  # dimed DFP
 
-    balanco_importer.import_balanco(69690)      # ITR dimed mes 09
+    # balanco_importer.import_balanco(69690)      # ITR dimed mes 09
     # balanco_importer.import_balanco(68182)      # ITR dimed mes 06
     # balanco_importer.import_balanco(65643)      # ITR dimed mes 03
 
