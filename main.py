47089c9d4c1dc259ec35fd1faf1c6fac3350c0de
Thiago Bocchile <tykoth@gmail.com>
2019-09-30 20:34:34 -0300

    Remoção do exists_ok

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
 from services import downloader_batch
 from services import balanco_importer
 
 
 def main():
-    # downloader_batch.download()
+    downloader_batch.download()
     # downloader_batch.download_list(dimed_list)
 
-    balanco_importer.import_all_balancos_from_company('009342')
+    # balanco_importer.import_all_balancos_from_company('009342')
 
     # balanco_importer.import_balanco(45446)  # dimed itr_1
     # balanco_importer.import_balanco(77224)  # dimed itr_2
